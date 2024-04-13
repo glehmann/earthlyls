1e78b7b8350addd3a7e36d5266bcb013fda5b472
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-04-13 09:43:41 +0200

    first try at go to reference feature

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::str::FromStr;
 
 use clean_path::Clean;
 use dashmap::DashMap;
-use earthlyls::queries::target_name;
-use earthlyls::util::{request_failed, ToLSPRange};
+use earthlyls::queries;
+use earthlyls::util::{request_failed, RopeProvider, ToLSPRange};
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
-use tree_sitter::{Node, Parser, Point};
+use tree_sitter::{Node, Parser, Point, QueryCursor};
 
 use earthlyls::descriptions::command_description;
 use earthlyls::document::Document;
@@ -87,6 +87,7 @@ impl LanguageServer for Backend {
             capabilities: ServerCapabilities {
                 definition_provider: Some(OneOf::Left(true)),
                 hover_provider: Some(HoverProviderCapability::Simple(true)),
+                references_provider: Some(OneOf::Left(true)),
                 text_document_sync: Some(TextDocumentSyncCapability::Options(
                     TextDocumentSyncOptions {
                         change: Some(TextDocumentSyncKind::INCREMENTAL),
@@ -172,7 +173,7 @@ impl LanguageServer for Backend {
     ) -> Result<Option<GotoDefinitionResponse>> {
         let pos = &params.text_document_position_params.position;
         let uri = &params.text_document_position_params.text_document.uri;
-        let doc = &self.docs.get(&uri).ok_or_else(|| request_failed("unknown document"))?;
+        let doc = &self.docs.get(&uri).ok_or_else(|| request_failed("unknown document: {uri}"))?;
         let root_node = doc.tree.root_node();
         let pos = Point { row: pos.line as usize, column: 1 + pos.character as usize };
         let mut cursor = root_node.walk();
@@ -196,7 +197,7 @@ impl LanguageServer for Backend {
                     .map_err(|_| request_failed("can't compute the earthfile path"))?;
                 let path = path
                     .parent()
-                    .ok_or_else(|| request_failed("can’t compute the current Earthfile parent"))?;
+                    .ok_or_else(|| request_failed("can't compute the current Earthfile parent"))?;
                 let path = path.join(earthfile).join("Earthfile").clean();
                 Url::from_file_path(path)
                     .map_err(|_| request_failed("can't convert the earthfile path to an url"))?
@@ -204,9 +205,11 @@ impl LanguageServer for Backend {
                 uri.to_owned()
             };
         let name = doc.node_content(name_node);
-        let target_doc =
-            &self.docs.get(&target_uri).ok_or_else(|| request_failed("unknown document"))?;
-        for node in target_doc.captures(target_name()) {
+        let target_doc = &self
+            .docs
+            .get(&target_uri)
+            .ok_or_else(|| request_failed(&format!("unknown document: {target_uri}")))?;
+        for node in target_doc.captures(queries::target_name()) {
             if target_doc.node_content(node) == name {
                 return Ok(Some(GotoDefinitionResponse::Scalar(Location {
                     uri: target_uri.to_owned(),
@@ -215,6 +218,106 @@ impl LanguageServer for Backend {
             }
         }
         Ok(None)
+    }
+
+    async fn references(&self, params: ReferenceParams) -> Result<Option<Vec<Location>>> {
+        let pos = &params.text_document_position.position;
+        let uri = &params.text_document_position.text_document.uri;
+        // let include_declaration = &params.context.include_declaration;
+        let doc = &self.docs.get(&uri).ok_or_else(|| request_failed("unknown document: {uri}"))?;
+        let pos = Point { row: pos.line as usize, column: pos.character as usize };
+
+        // some query stuff
+        let query = queries::target_or_function_ref();
+        let ref_idx = query.capture_index_for_name("ref").unwrap();
+        let target_earthfile_idx = query.capture_index_for_name("target_earthfile").unwrap();
+        let target_name_idx = query.capture_index_for_name("target_name").unwrap();
+
+        // find the query match at the given position
+        let mut query_cursor = QueryCursor::new();
+        let mut matches =
+            query_cursor.matches(query, doc.tree.root_node(), RopeProvider(doc.rope.slice(..)));
+        let Some(m) = matches.find(|m| {
+            let node = m.nodes_for_capture_index(ref_idx).nth(0).unwrap();
+            node.start_position() <= pos && pos < node.end_position()
+        }) else {
+            return Ok(None);
+        };
+
+        // extract the target name from the capture
+        let Some(name_capture) = m.captures.iter().filter(|c| c.index == target_name_idx).nth(0)
+        else {
+            return Ok(None);
+        };
+        let target_name = doc.node_content(name_capture.node);
+
+        // extract the earthfile uri
+        let earthfile_capture =
+            m.captures.iter().filter(|c| c.index == target_earthfile_idx).nth(0);
+        let target_uri = if let Some(earthfile_capture) = earthfile_capture {
+            let earthfile = doc.node_content(earthfile_capture.node);
+            let path = PathBuf::from_str(uri.path())
+                .map_err(|_| request_failed("can't compute the earthfile path"))?;
+            let path = path
+                .parent()
+                .ok_or_else(|| request_failed("can't compute the current Earthfile parent"))?;
+            let path = path.join(earthfile).join("Earthfile").clean();
+            Url::from_file_path(path)
+                .map_err(|_| request_failed("can't convert the earthfile path to an url"))?
+        } else {
+            uri.to_owned()
+        };
+        eprintln!("{target_uri}");
+
+        // now search in all the known documents to find some references to that target in that earthfile
+        let mut res: Vec<Location> = Vec::new();
+        for item in self.docs.iter() {
+            let other_uri = item.key();
+            let other_doc = item.value();
+            let matches = query_cursor.matches(
+                query,
+                other_doc.tree.root_node(),
+                RopeProvider(other_doc.rope.slice(..)),
+            );
+            for m in matches {
+                // extract the target name from the capture
+                let Some(name_capture) =
+                    m.captures.iter().filter(|c| c.index == target_name_idx).nth(0)
+                else {
+                    continue;
+                };
+                let ref_name = other_doc.node_content(name_capture.node);
+
+                // extract the earthfile uri
+                let earthfile_capture =
+                    m.captures.iter().filter(|c| c.index == target_earthfile_idx).nth(0);
+                let ref_uri = if let Some(earthfile_capture) = earthfile_capture {
+                    let earthfile = other_doc.node_content(earthfile_capture.node);
+                    let path = PathBuf::from_str(other_uri.path())
+                        .map_err(|_| request_failed("can't compute the earthfile path"))?;
+                    let path = path.parent().ok_or_else(|| {
+                        request_failed("can't compute the current Earthfile parent")
+                    })?;
+                    let path = path.join(earthfile).join("Earthfile").clean();
+                    Url::from_file_path(path)
+                        .map_err(|_| request_failed("can't convert the earthfile path to an url"))?
+                } else {
+                    other_uri.to_owned()
+                };
+                if ref_uri == target_uri && ref_name == target_name {
+                    let range = if let Some(ref_capture) =
+                        m.captures.iter().filter(|c| c.index == ref_idx).nth(0)
+                    {
+                        ref_capture.node.range()
+                    } else {
+                        name_capture.node.range()
+                    };
+
+                    res.push(Location { uri: other_uri.to_owned(), range: range.to_lsp_range() });
+                }
+            }
+        }
+        Ok(Some(res))
     }
 }
 
--- src/queries.rs
+++ src/queries.rs
@@ -8,3 +8,20 @@ pub fn target_name() -> &'static Query {
         Query::new(&crate::parser::language(), r"(target name: (identifier) @target_name)").unwrap()
     })
 }
+
+pub fn target_or_function_ref() -> &'static Query {
+    static QUERY: OnceLock<Query> = OnceLock::new();
+    QUERY.get_or_init(|| {
+        Query::new(
+            &crate::parser::language(),
+            r"(target_ref
+                earthfile: (earthfile_ref)? @target_earthfile
+                name: (identifier) @target_name) @ref
+              (function_ref
+                earthfile: (earthfile_ref)? @target_earthfile
+                name: (identifier) @target_name) @ref
+              ",
+        )
+        .unwrap()
+    })
+}
