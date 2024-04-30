051a138f28552a475bbf8979799a0fdf89e65e12
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-04-30 15:25:42 +0200

    goto_definition: support for the new wildcard in earthfile reference

--- src/commands/go_to_definition.rs
+++ src/commands/go_to_definition.rs
@@ -1,6 +1,8 @@
 use std::{path::PathBuf, str::FromStr};
 
 use clean_path::Clean;
+// use glob_match::glob_match;
+use glob_match::glob_match;
 use tower_lsp::{jsonrpc::Result, lsp_types::*};
 use tree_sitter::{Node, Point};
 
@@ -33,34 +35,45 @@ pub fn goto_definition(
     let Some(name_node) = origin_node.child_by_field_name("name") else {
         return Ok(None);
     };
-    let target_uri = if let Some(earthfile_ref_node) = origin_node.child_by_field_name("earthfile")
+    let target_uris = if let Some(earthfile_ref_node) = origin_node.child_by_field_name("earthfile")
     {
-        let earthfile = doc.node_content(earthfile_ref_node);
-        let path = PathBuf::from_str(uri.path())
-            .map_err(|_| request_failed("can't compute the earthfile path"))?;
-        let path = path
-            .parent()
-            .ok_or_else(|| request_failed("can't compute the current Earthfile parent"))?;
-        let path = path.join(earthfile).join("Earthfile").clean();
-        Url::from_file_path(path)
-            .map_err(|_| request_failed("can't convert the earthfile path to an url"))?
+        let earthfile_ref = doc.node_content(earthfile_ref_node);
+        match_earthfile_ref(backend, uri, &earthfile_ref)?
     } else {
-        uri.to_owned()
+        vec![uri.to_owned()]
     };
     let name = doc.node_content(name_node);
-    let target_doc = &backend
-        .docs
-        .get(&target_uri)
-        .ok_or_else(|| request_failed(&format!("unknown document: {target_uri}")))?;
-    for node in target_doc.captures(queries::target_name()) {
-        if target_doc.node_content(node) == name {
-            return Ok(Some(GotoDefinitionResponse::Link(vec![LocationLink {
-                origin_selection_range: Some(origin_node.range().to_lsp_range()),
-                target_uri: target_uri.to_owned(),
-                target_range: node.range().to_lsp_range(), // TODO: this should probably be a different range
-                target_selection_range: node.range().to_lsp_range(),
-            }])));
+    let mut res = Vec::new();
+    for target_uri in target_uris {
+        let target_doc = &backend
+            .docs
+            .get(&target_uri)
+            .ok_or_else(|| request_failed(&format!("unknown document: {target_uri}")))?;
+        for node in target_doc.captures(queries::target_name()) {
+            if target_doc.node_content(node) == name {
+                res.push(LocationLink {
+                    origin_selection_range: Some(origin_node.range().to_lsp_range()),
+                    target_uri: target_uri.to_owned(),
+                    target_range: node.range().to_lsp_range(), // TODO: this should probably be a different range
+                    target_selection_range: node.range().to_lsp_range(),
+                });
+            }
         }
     }
-    Ok(None)
+    Ok(Some(GotoDefinitionResponse::Link(res)))
+}
+
+fn match_earthfile_ref(backend: &Backend, origin: &Url, earthfile_ref: &str) -> Result<Vec<Url>> {
+    let path = PathBuf::from_str(origin.path())
+        .map_err(|_| request_failed("can't compute the earthfile path"))?;
+    let path = path
+        .parent()
+        .ok_or_else(|| request_failed("can't compute the current Earthfile parent"))?;
+    let path = path.join(earthfile_ref).join("Earthfile").clean().to_string_lossy().to_string();
+    Ok(backend
+        .docs
+        .iter()
+        .map(|i| i.key().clone())
+        .flat_map(|uri| if glob_match(&path, uri.path()) { Some(uri) } else { None })
+        .collect())
 }
