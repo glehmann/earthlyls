0efe7e3ffabb7f27d6613a2ddcb851827a35e790
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-05-03 09:32:43 +0200

    more hover tests

--- tests/hover.rs
+++ tests/hover.rs
@@ -24,3 +24,54 @@ async fn should_hover_with_keyword() {
     assert!(markup.value.contains("VERSION"));
     // panic!("Don’t panic!");
 }
+
+#[tokio::test]
+async fn should_hover_with_non_command_keyword() {
+    let mut ctx = TestContext::new();
+    ctx.initialize().await;
+    let res = ctx
+        .request::<request::HoverRequest>(HoverParams {
+            text_document_position_params: TextDocumentPositionParams {
+                position: Position { line: 7, character: 24 },
+                text_document: TextDocumentIdentifier { uri: ctx.doc_uri("Earthfile") },
+            },
+            work_done_progress_params: WorkDoneProgressParams { work_done_token: None },
+        })
+        .await
+        .unwrap();
+    let HoverContents::Markup(markup) = res.contents else { panic!("not a markup content!") };
+    assert!(markup.value.contains("SAVE ARTIFACT"));
+    // panic!("Don’t panic!");
+}
+
+#[tokio::test]
+async fn should_not_hover_outside_keyword() {
+    let mut ctx = TestContext::new();
+    ctx.initialize().await;
+    let res = ctx
+        .request::<request::HoverRequest>(HoverParams {
+            text_document_position_params: TextDocumentPositionParams {
+                position: Position { line: 0, character: 7 },
+                text_document: TextDocumentIdentifier { uri: ctx.doc_uri("Earthfile") },
+            },
+            work_done_progress_params: WorkDoneProgressParams { work_done_token: None },
+        })
+        .await;
+    assert!(res.is_none());
+}
+
+#[tokio::test]
+async fn should_not_hover_on_eol() {
+    let mut ctx = TestContext::new();
+    ctx.initialize().await;
+    let res = ctx
+        .request::<request::HoverRequest>(HoverParams {
+            text_document_position_params: TextDocumentPositionParams {
+                position: Position { line: 0, character: 29 },
+                text_document: TextDocumentIdentifier { uri: ctx.doc_uri("Earthfile") },
+            },
+            work_done_progress_params: WorkDoneProgressParams { work_done_token: None },
+        })
+        .await;
+    assert!(res.is_none());
+}
