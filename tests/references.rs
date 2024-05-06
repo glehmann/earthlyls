02e606d6d97c9f4361ded71ef092070a91f518a3
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-05-06 12:43:53 +0200

    test references without declaration

--- tests/references.rs
+++ tests/references.rs
@@ -48,3 +48,32 @@ async fn should_provide_references() {
     assert_eq!(r.range.end.character, 15);
     // panic!("Don’t panic!");
 }
+
+#[tokio::test]
+async fn should_provide_references_without_declaration() {
+    let mut ctx = TestContext::new();
+    ctx.initialize().await;
+    let mut res = ctx
+        .request::<request::References>(ReferenceParams {
+            context: ReferenceContext { include_declaration: false },
+            partial_result_params: PartialResultParams { partial_result_token: None },
+            text_document_position: TextDocumentPositionParams {
+                position: Position { line: 2, character: 2 },
+                text_document: TextDocumentIdentifier { uri: ctx.doc_uri("Earthfile") },
+            },
+            work_done_progress_params: WorkDoneProgressParams { work_done_token: None },
+        })
+        .await
+        .unwrap();
+    res.sort_by_key(|l| (l.uri.to_string(), l.range.start.line));
+    assert_eq!(res.len(), 3);
+    let r = &res[0];
+    assert_eq!(r.uri, ctx.doc_uri("Earthfile"));
+    assert_eq!(r.range.start.line, 6);
+    assert_eq!(r.range.start.character, 7);
+    let r = &res[1];
+    assert_eq!(r.uri, ctx.doc_uri("bar/Earthfile"));
+    let r = &res[2];
+    assert_eq!(r.uri, ctx.doc_uri("foo/Earthfile"));
+    // panic!("Don’t panic!");
+}
