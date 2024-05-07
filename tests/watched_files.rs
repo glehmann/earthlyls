5dbbf99b9808989719f8b8b29a1919749322c316
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-05-07 22:47:35 +0200

    test deleted notification

--- tests/watched_files.rs
+++ tests/watched_files.rs
@@ -86,3 +86,33 @@ new-target:
     assert_eq!(res.len(), 4);
     // panic!("Don’t panic!");
 }
+
+#[tokio::test]
+async fn should_react_to_deleted_notification() {
+    let mut ctx = TestContext::new();
+    ctx.initialize().await;
+
+    std::fs::remove_file(ctx.doc_uri("bar/Earthfile").to_file_path().unwrap()).unwrap();
+    ctx.notify::<notification::DidChangeWatchedFiles>(DidChangeWatchedFilesParams {
+        changes: vec![FileEvent {
+            uri: ctx.doc_uri("bar/Earthfile"),
+            typ: FileChangeType::DELETED,
+        }],
+    })
+    .await;
+
+    let res = ctx
+        .request::<request::References>(ReferenceParams {
+            context: ReferenceContext { include_declaration: false },
+            partial_result_params: PartialResultParams { partial_result_token: None },
+            text_document_position: TextDocumentPositionParams {
+                position: Position { line: 2, character: 0 },
+                text_document: TextDocumentIdentifier { uri: ctx.doc_uri("Earthfile") },
+            },
+            work_done_progress_params: WorkDoneProgressParams { work_done_token: None },
+        })
+        .await
+        .unwrap();
+    assert_eq!(res.len(), 2);
+    // panic!("Don’t panic!");
+}
