426a67c9b6a79450137afbde1d71b7b8e51fdb83
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-05-25 14:15:43 +0200

    test --build-arg deprecation diagnostic

--- tests/diagnostic.rs
+++ tests/diagnostic.rs
@@ -11,23 +11,30 @@ async fn should_publish_syntax_diagnostics() {
     let dp = ctx.recv::<PublishDiagnosticsParams>().await;
     assert_eq!(dp.uri, ctx.doc_uri("Earthfile"));
     let ds = dp.diagnostics;
-    assert_eq!(ds.len(), 3);
+    assert_eq!(ds.len(), 4);
 
     let d = &ds[0];
+    assert_eq!(d.range.start.line, 6);
+    assert_eq!(d.range.start.character, 8);
+    assert_eq!(d.range.end.line, 6);
+    assert_eq!(d.range.end.character, 27);
+    assert!(d.message.contains("deprecated"));
+
+    let d = &ds[1];
     assert_eq!(d.range.start.line, 3);
     assert_eq!(d.range.start.character, 5);
     assert_eq!(d.range.end.line, 3);
     assert_eq!(d.range.end.character, 21);
     assert_eq!(d.message, "unknown option");
 
-    let d = &ds[1];
+    let d = &ds[2];
     assert_eq!(d.range.start.line, 4);
     assert_eq!(d.range.start.character, 2);
     assert_eq!(d.range.end.line, 4);
     assert_eq!(d.range.end.character, 6);
     assert_eq!(d.message, "syntax error");
 
-    let d = &ds[2];
+    let d = &ds[3];
     assert_eq!(d.range.start.line, 5);
     assert_eq!(d.range.start.character, 11);
     assert_eq!(d.range.end.line, 5);
